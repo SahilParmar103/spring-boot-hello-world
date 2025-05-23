2ea2a5a292c59c880cfa0a28a91a4b9fcda0c3ab
Sahil Parmar <98935505+SahilParmar103@users.noreply.github.com>
2025-05-22 21:35:17 -0400

    Delete scripts/start.sh

--- scripts/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Stopping running Java application..."
-PID=$(pgrep -f 'target/.*\.jar')
-if [ -n "$PID" ]; then
-  kill $PID
-  echo "Stopped process $PID"
-else
-  echo "No Java process found."
-fi
