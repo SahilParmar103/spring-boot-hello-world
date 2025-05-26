4cf1b387c5ac7bf961f0eaad28f50caead874898
Sahil Parmar <98935505+SahilParmar103@users.noreply.github.com>
2025-05-26 08:32:32 -0400

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -2,7 +2,7 @@
 echo "Stopping running Java application..."
 PID=$(pgrep -f 'target/.*\.jar')
 if [ -n "$PID" ]; then
-  kill $PID
+  kill $PID || true
   echo "Stopped process $PID"
 else
   echo "No Java process found."
