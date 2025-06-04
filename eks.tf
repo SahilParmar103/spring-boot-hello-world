04c225790931de2197566d5c9ea9980426a696db
Sahil Parmar <98935505+SahilParmar103@users.noreply.github.com>
2025-06-04 07:48:31 -0400

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -12,9 +12,9 @@ terraform {
     bucket = "bucketforazur"
     key    = "eks/terraform.tfstate"
     region = "us-east-1"
-    assume_role = {
-      role_arn = "arn:aws:iam::474668397798:role/S3Role"
-    }
+    #assume_role = {
+     # role_arn = "arn:aws:iam::474668397798:role/S3Role"
+    #}
   }
 }
 
